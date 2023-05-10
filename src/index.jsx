5556c8fe2736152535149a7a55618c27809230e3
MadFlip <mykola@bleech.de>
2023-05-10 10:32:20 +0200

    feat(src): basic structure, jsx syntax, styles

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,19 @@ import './style.css'
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.querySelector('#root'))
+const toto = 0
 
 root.render(
-    <h1>Hello React</h1>
+  <>
+    <h1 className="title" style={
+      {
+        color: 'var(--color-primary)',
+        fontSize: '42px',
+        textTransform: 'uppercase'
+      }
+    }>Hello { toto ? toto : 'empty' }</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit <br />Blanditiis corrupti deserunt consectetur aut, quo ad molestiae, a incidunt, repudiandae ea sapiente impedit inventore ex similique at? Consequuntur adipisci voluptate recusandae?</p>
+    <input type="checkbox" id="test" />
+    <label htmlFor="test">That checkbox</label>
+  </>
 )
