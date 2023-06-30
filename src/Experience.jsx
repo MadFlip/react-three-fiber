bd45123f906e4406e63574c1566e991e9cad0417
Mykola <mykola@bleech.de>
2023-06-30 16:11:07 +0200

    feat(Experience.jsx): add  OrbitControls, Lights

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,28 +1,36 @@
-import { useFrame } from "@react-three/fiber"
+import { extend, useFrame, useThree } from "@react-three/fiber"
 import { useRef } from "react"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+
+extend({ OrbitControls })
 
 export default function Experience() {
   const groupRef = useRef()
   const cubeRef = useRef()
+  const { camera, gl } = useThree()
 
   useFrame((state, delta) => {
     groupRef.current.rotation.y += delta * 2
-    cubeRef.current.rotation.y += delta * 2
   })
 
   return <>
+    <orbitControls args={[ camera, gl.domElement ]} />
+    <directionalLight position={[ 1, 2, 3 ]} intensity={ 1.5 } />
+    <ambientLight intensity={ 0.5 } />
+
     <mesh position-y={ -1 } rotation-x={ -Math.PI * 0.5 }>
       <planeGeometry args={[5, 5]} />
-      <meshBasicMaterial color='#0f0'/>
+      <meshStandardMaterial color='#0f0'/>
     </mesh>
+
     <group ref={ groupRef }>
       <mesh position-x={ -2 }>
         <sphereGeometry args={[1, 16, 16]}/>
-        <meshBasicMaterial color='#f00' />
+        <meshStandardMaterial color='#f00' />
       </mesh>
       <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.25 } position-x={ 1.5 } scale={ 0.5 }>
         <boxGeometry args={[2, 2]} />
-        <meshBasicMaterial color='#ff0' />
+        <meshStandardMaterial color='#ff0' />
       </mesh>
     </group>
   </>
