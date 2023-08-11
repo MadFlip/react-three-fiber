7915998f6eab9f3b56cee960333dd183a752f643
Mykola <mykola@bleech.de>
2023-08-11 17:59:57 +0200

    feat(Experience.jsx): useLoader to load model, DracoLoader

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,139 +1,30 @@
-import { Lightformer, Environment, OrbitControls, useHelper } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
-import { useControls, button } from 'leva'
+import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useRef } from 'react'
-import * as THREE from 'three'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 
 export default function Experience()
 {
-    const { perfVisible } = useControls('Performance', {
-      perfVisible: false,
+    const model = useLoader(GLTFLoader, './hamburger-draco.glb', loader => {
+        const dracoLoader = new DRACOLoader()
+        dracoLoader.setDecoderPath('./draco/')
+        loader.setDRACOLoader(dracoLoader)
     })
-
-    const { position, color, visible, choice } = useControls('Sphere',{
-      position: {
-        value: { x: -2, y: 0.2},
-        x: { min: -5, max: 5, step: 0.1 },
-        y: { min: 0.2, max: 5, step: 0.1 },
-        joystick: 'invertY'
-      },
-      color: 'white',
-      visible: true,
-      myInterval: {
-        min: 0,
-        max: 10,
-        value: [ 4, 5 ],
-      },
-      clickMe: button(() => console.log('clicked')),
-      choice: { label: 'scale', options: [ 1, 1.5, 2 ], value: 1}
-    })
-
-    const { scale } = useControls('Box', {
-      scale: {
-        value: 1,
-        min: 0,
-        max: 2,
-        step: 0.1,
-      }
-    })
-
-    const { shadowColor, shadowOpacity, shadowBlur, shadowPosition } = useControls('ControlShadows',{
-      shadowOpacity: {
-        value: 0.5,
-        min: 0,
-        max: 1,
-        step: 0.1,
-      },
-      shadowColor: '#211614',
-      shadowBlur: {
-        value: 1,
-        min: 0,
-        max: 10,
-        step: 0.1,
-      },
-      shadowPosition: {
-        value: { x: 0, y: 0},
-        x: { min: -5, max: 5, step: 0.1 },
-        y: { min: -5, max: 5, step: 0.1 },
-        joystick: 'invertY'
-      },
-    })
-
-    const { envMapIntensity } = useControls('Environment', {
-      envMapIntensity: {
-        value: 1,
-        min: 0,
-        max: 10,
-        step: 0.1,
-      },
-    })
-
-    const cube = useRef()
-
-    useFrame((state, delta) => {
-      cube.current.rotation.y += delta * 0.2
-    })
-
     return <>
-        {/* <BakeShadows />
-        <SoftShadows frustum={ 3.75 }
-          size={ 50 }
-          near={ 9.5 }
-          samples={ 17 }
-          rings={ 11 }
-        /> */}
-        <Environment 
-          background
-          preset='sunset'
-          resolution={ 32 }
-        >
-          <color args={ ['blue'] } attach="background" />
-          <Lightformer position={ -5 }
-            scale={ 5 }
-            color={ 'red' }
-            intensity={ 10 }
-            form="ring"
-          ></Lightformer>
-          {/* <mesh position-z={ -5 } scale={ 10 }> 
-            <planeGeometry/>
-            <meshBasicMaterial color={ [ 10, 0, 0 ] }/>
-          </mesh> */}
-        </Environment>
 
-        
-        
-        <color args={[ 'ivory' ]} attach="background" />
+        <Perf position="top-left" />
 
-        { perfVisible && <Perf openByDefault trackGPU={ true } position="bottom-right" /> }
         <OrbitControls makeDefault />
 
-        {/* <ContactShadows
-          position={[ shadowPosition.x, -0.99, shadowPosition.y ]}
-          scale={ 10 }
-          resolution={ 512 }
-          far={ 5 }
-          color = { shadowColor }
-          opacity = { shadowOpacity }
-          blur = { position.y }
-          frames={ 1 }
-        /> */}
-    
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
 
-        <mesh castShadow position={[ position.x, position.y, 0 ]} scale={ choice }  visible={ visible }>
-            <sphereGeometry />
-            <meshStandardMaterial color={ color } envMapIntensity={ envMapIntensity }/>
-        </mesh>
-
-        <mesh ref={ cube } castShadow position-x={ 2 } scale={ scale }>
-            <boxGeometry />
-            <meshStandardMaterial color="white"  envMapIntensity={ envMapIntensity }/>
-        </mesh>
-
-        <mesh position-y={ -1 } rotation-x={ -Math.PI * 0.5 } scale={ 10 }>
+        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            <meshStandardMaterial color="greenyellow"  envMapIntensity={ envMapIntensity }/>
+            <meshStandardMaterial color="greenyellow" />
         </mesh>
 
+        <primitive object={ model.scene } scale={ 0.35 } />
     </>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,28 +2,19 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
-import { StrictMode } from 'react'
-import { Leva } from 'leva'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
-// const created = ({ gl, scene }) => {
-//   gl.setClearColor('red')
-//   scene.background = new THREE.Color('blue')
-// }
 
 root.render(
-  <StrictMode>
     <Canvas
-      shadows={ false }
-      camera={{
-          fov: 45,
-          near: 0.1,
-          far: 200,
-          position: [-4, 3, 6]
-      }}
-      // onCreated={ created }
+        shadows
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ - 4, 3, 6 ]
+        } }
     >
-      <Experience />
+        <Experience />
     </Canvas>
-  </StrictMode>
 )
