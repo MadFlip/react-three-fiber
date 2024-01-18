72b77001dd3f7074d1b3e225314b0859db3745b7
Mykola <mykola@bleech.de>
2024-01-18 18:51:02 +0100

    feat(Hamburger): add hamburger model, with meshes as components, test Clone

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,7 +2,7 @@ import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { Suspense } from 'react'
 import Placeholder from './Placeholder.jsx'
-import Model from './Model.jsx'
+import Hamburger from './Hamburger.jsx'
 
 export default function Experience()
 {
@@ -12,7 +12,7 @@ export default function Experience()
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } shadow-normalBias={ 0.04 } />
         <ambientLight intensity={ 0.5 } />
 
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
@@ -20,7 +20,7 @@ export default function Experience()
             <meshStandardMaterial color="greenyellow" />
         </mesh>
         <Suspense fallback={<Placeholder position-y={ 0.6 } scale={[2, 1, 2]} />}>
-            <Model />
+            <Hamburger scale={ 0.35 } />
         </Suspense>
     </>
 }
