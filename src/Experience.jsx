b129f566269c30922f294644e0b6a0844807c389
Mykola <mykola@bleech.de>
2024-01-19 16:14:56 +0100

    feat(PortalScene): plying with sky, adding shaderMaterial to the base cube

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,5 +1,6 @@
-import { shaderMaterial, Sparkles, useTexture, useGLTF, OrbitControls, Center } from '@react-three/drei'
-import { extend } from '@react-three/fiber'
+import { shaderMaterial, Sparkles, useTexture, useGLTF, OrbitControls, Center, Sky } from '@react-three/drei'
+import { useFrame, extend } from '@react-three/fiber'
+import { useRef } from 'react'
 import portalVertexShader from './shaders/portal/vertex.glsl'
 import portalFragmentShader from './shaders/portal/fragment.glsl'
 import * as THREE from 'three'
@@ -7,8 +8,8 @@ import * as THREE from 'three'
 const PortalMaterial = shaderMaterial(
   {
     uTime: 0,
-    uColorStart: new THREE.Color('#ff7d46'),
-    uColorEnd: new THREE.Color('#f9d423'),
+    uColorStart: new THREE.Color('#fbeabb'),
+    uColorEnd: new THREE.Color('#f8c73f'),
   },
   portalVertexShader,
   portalFragmentShader
@@ -22,26 +23,51 @@ export default function Experience()
   const bakedTexture = useTexture('./model/baked.jpg')
   bakedTexture.flipY = false
 
+  const portalMaterialRef = useRef()
+  const portalMaterialRef2 = useRef()
+
+  const baseHeight = 2.1
+
+  useFrame((state, delta) => {
+    portalMaterialRef.current.uTime += delta
+    portalMaterialRef2.current.uTime += delta
+  })
+
   return <>
     <color args={[ '#030202' ]} attach='background' />
+    <Sky
+      distance={ 4 }
+      inclination={ 0.4 }
+      azimuth={ 0.25 }
+      rayleigh={ 10 }
+      mieCoefficient={ 0.005 }
+      mieDirectionalG={ 0.7 }
+      turbidity={ 10 }
+    />
 
-    <OrbitControls makeDefault />
-
+    <OrbitControls makeDefault
+      autoRotate
+    />
     <Center>
       <mesh geometry={ nodes.baked.geometry }>
         <meshBasicMaterial map={ bakedTexture } />
       </mesh>
 
+      <mesh position-y={ baseHeight * -1/2 - 0.0003 }>
+        <boxGeometry args={[4, baseHeight, 4]} />
+        <portalMaterial ref={ portalMaterialRef2 } uColorStart={ new THREE.Color('#ba9f34') } uColorEnd={ new THREE.Color('#ac7b1e') }  />
+      </mesh>
+
       <mesh geometry={ nodes.poleLightA.geometry } position={ nodes.poleLightA.position }>
-        <meshBasicMaterial color='#ff7d46' />
+        <meshBasicMaterial color='#fbeabb' />
       </mesh>
 
       <mesh geometry={ nodes.poleLightB.geometry } position={ nodes.poleLightB.position }>
-        <meshBasicMaterial color='#ff7d46' />
+        <meshBasicMaterial color='#fbeabb' />
       </mesh>
 
       <mesh geometry={ nodes.portalLight.geometry } position={ nodes.portalLight.position } rotation={ nodes.portalLight.rotation }>
-        <portalMaterial />
+        <portalMaterial ref={ portalMaterialRef } />
       </mesh>
     
       <Sparkles
