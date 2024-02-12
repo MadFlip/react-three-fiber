57700cb035f51d21256f5e9560ad0c5ff4381c32
Mykola <mykola@bleech.de>
2024-02-12 09:44:21 +0100

    feat(Physics): install rapier, use RigidBody and Physics, colliders ball, debug mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
 import './style.css'
-import App from './App'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <Canvas
+        shadows
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ 4, 2, 6 ]
+        } }
+    >
+        <Experience />
+    </Canvas>
+)
