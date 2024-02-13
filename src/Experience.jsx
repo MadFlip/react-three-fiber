08fb165cdfa5661f13fdbd939af262b884184358
Mykola <mykola@bleech.de>
2024-02-13 12:57:15 +0100

    feat(Physics): different type of colliders

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -13,21 +13,22 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
         
-        <Physics debug>
+        <Physics 
+        // debug
+        >
           <RigidBody colliders="ball">
-            <mesh castShadow position={ [ -2, 2, 0 ] }>
+            <mesh castShadow position={ [ 0, 3, 0.5 ] }>
               <sphereGeometry />
               <meshStandardMaterial color="orange" />
             </mesh>
           </RigidBody>
+          {/* colliders 'ball' wraps the mesin in a sphere */}
+          {/* colliders 'hull' wraps the mesh like a coat */}
+          {/* colliders 'trimesh' wraps more precisely, including cavities and holes */}
 
-          <RigidBody>
-            <mesh castShadow position={ [ 2, 2, 0 ] }>
-              <boxGeometry args={[ 3, 2, 1 ]}/>
-              <meshStandardMaterial color="mediumpurple" />
-            </mesh>
-            <mesh castShadow position={ [ 2, 2, 1 ] }>
-              <boxGeometry args={[ 1, 1, 1 ]}/>
+          <RigidBody colliders="trimesh">
+            <mesh castShadow position={[ 0, 1, 0 ]} rotation={[ Math.PI * 0.1, 0, 0 ]}>
+              <torusGeometry args={[ 1, 0.5, 16, 32 ]} />
               <meshStandardMaterial color="mediumpurple" />
             </mesh>
           </RigidBody>
