42963ee6f7edb5e8ac1e8dde87432d449b99f8e6
Mykola <mykola@bleech.de>
2024-02-14 10:37:59 +0100

    feat(Physics): gravity, gravityScale, restitution, friction, mass control on custom collider

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,24 @@
-import { Capsule, OrbitControls } from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { CuboidCollider, BallCollider, CapsuleCollider, RigidBody, Physics } from '@react-three/rapier'
+import { RigidBody, Physics, CuboidCollider } from '@react-three/rapier'
+import { useRef } from 'react' 
 
 export default function Experience()
 {
+    const cube = useRef()
+
+    const cubeJump = () => {
+      const mass = cube.current.mass()
+      console.log('Cube mass:', mass)
+
+      cube.current.applyImpulse({ x: 0, y: 5 * mass, z: 0 })
+      cube.current.applyTorqueImpulse({ 
+        x: Math.random() - 0.5,
+        y: Math.random() - 0.5,
+        z: Math.random() - 0.5
+      })
+    }
+
     return <>
 
         <Perf position="top-left" />
@@ -13,11 +28,12 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
         
-        <Physics 
-        debug
+        <Physics
+          debug
+          gravity={ [ 0, -9.81, 0 ] }
         >
-          <RigidBody colliders="ball">
-            <mesh castShadow position={ [ 0, 3, 0.5 ] }>
+          <RigidBody colliders="ball" restitution={ 0.8 }>
+            <mesh castShadow position={ [ -1.5, 2, 0 ] }>
               <sphereGeometry />
               <meshStandardMaterial color="orange" />
             </mesh>
@@ -26,17 +42,26 @@ export default function Experience()
           {/* colliders 'hull' wraps the mesh like a coat */}
           {/* colliders 'trimesh' wraps more precisely, including cavities and holes */}
 
-          <RigidBody colliders={ false } position={[ 0, 1, 0 ]} rotation={[ Math.PI * 0.2, 0, 0 ]}>
-            {/* <CuboidCollider args={[1.5, 1.5, 0.5]} /> */}
-            {/* <BallCollider args={[1.5]} /> */}
-            <CapsuleCollider args={[1.5, 0.5]} />
-            <mesh castShadow>
-              <torusGeometry args={[ 1, 0.5, 16, 32 ]} />
+          <RigidBody ref={ cube } position={[ 1.5, 2, 0 ]} 
+            gravityScale={ 0.8 } 
+            restitution={ 0.8 }
+            friction={ 0 }
+            colliders={ false }
+            >
+            <mesh castShadow
+              onClick={ cubeJump }
+              >
+              <boxGeometry />
               <meshStandardMaterial color="mediumpurple" />
             </mesh>
+            {/* To control mass we need to use custom collider */}
+            <CuboidCollider mass={ 0.1 } args={[ 0.5, 0.5, 0.5 ]} />
           </RigidBody>
 
-          <RigidBody type="fixed">
+          <RigidBody type="fixed" 
+            // restitution={ 1 }
+            friction={ 1 }
+            >
             <mesh receiveShadow position-y={ - 1.25 }>
               <boxGeometry args={ [ 10, 0.5, 10 ] } />
               <meshStandardMaterial color="greenyellow" />
