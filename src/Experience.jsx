172252785cc34c5e7cf2f259146abe54818fbfa4
Mykola <mykola@bleech.de>
2024-02-14 10:49:48 +0100

    feat(Physics): KinematicRotation, KinematicTranslation using quaternion

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,10 +2,13 @@ import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { RigidBody, Physics, CuboidCollider } from '@react-three/rapier'
 import { useRef } from 'react' 
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
 
 export default function Experience()
 {
     const cube = useRef()
+    const twister = useRef()
 
     const cubeJump = () => {
       const mass = cube.current.mass()
@@ -18,6 +21,19 @@ export default function Experience()
         z: Math.random() - 0.5
       })
     }
+
+    useFrame((state) => {
+      const time = state.clock.getElapsedTime()
+      const eulerRotation = new THREE.Euler(0, time * 4, 0)
+      const quaternionRotation = new THREE.Quaternion()
+      quaternionRotation.setFromEuler(eulerRotation)
+      twister.current.setNextKinematicRotation(quaternionRotation)
+
+      const angle = time * 0.5
+      const x = Math.cos(angle) * 2
+      const z = Math.sin(angle) * 2
+      twister.current.setNextKinematicTranslation({ x, y: -0.8, z })
+    })
 
     return <>
 
@@ -55,7 +71,7 @@ export default function Experience()
               <meshStandardMaterial color="mediumpurple" />
             </mesh>
             {/* To control mass we need to use custom collider */}
-            <CuboidCollider mass={ 0.1 } args={[ 0.5, 0.5, 0.5 ]} />
+            <CuboidCollider mass={ 1 } args={[ 0.5, 0.5, 0.5 ]} />
           </RigidBody>
 
           <RigidBody type="fixed" 
@@ -65,6 +81,18 @@ export default function Experience()
             <mesh receiveShadow position-y={ - 1.25 }>
               <boxGeometry args={ [ 10, 0.5, 10 ] } />
               <meshStandardMaterial color="greenyellow" />
+            </mesh>
+          </RigidBody>
+
+          <RigidBody
+            ref={ twister }
+            position={[ 0, -0.8, 0 ]}
+            friction={ 0 }
+            type="kinematicPosition"
+            >
+            <mesh castShadow scale={[ 0.4, 0.4, 3 ]}>
+              <boxGeometry />
+              <meshStandardMaterial color="red" />
             </mesh>
           </RigidBody>
         </Physics>
