e4a3e00fedeffb727fb4544fb0f50d5efa9f38fc
Mykola <mykola@bleech.de>
2024-02-15 17:18:26 +0100

    feat(Game): finish level physics

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,7 +1,8 @@
 import * as THREE from 'three'
-import { RigidBody } from '@react-three/rapier'
-import { useRef, useState } from 'react'
+import { CuboidCollider, RigidBody } from '@react-three/rapier'
+import { useMemo, useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
 const floor1Material = new THREE.MeshStandardMaterial({ color: 'limegreen' })
@@ -9,7 +10,7 @@ const floor2Material = new THREE.MeshStandardMaterial({ color: 'greenyellow' })
 const obstacleMaterial = new THREE.MeshStandardMaterial({ color: 'orangered' })
 const wallMaterial = new THREE.MeshStandardMaterial({ color: 'slategrey' })
 
-function BlockStart ({ position = [0, 0, 0] }) {
+export function BlockStart ({ position = [0, 0, 0] }) {
   return <group position={ position }>
     {/* Floor */}
     <mesh geometry={ boxGeometry } material={ floor1Material } position-y={ 0 }
@@ -17,7 +18,7 @@ function BlockStart ({ position = [0, 0, 0] }) {
   </group>
 }
 
-function BlockSpinner ({ position = [0, 0, 0] }) {
+export function BlockSpinner ({ position = [0, 0, 0] }) {
   const obstacle = useRef()
   const [ speed ] = useState(() => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1))
 
@@ -42,7 +43,7 @@ function BlockSpinner ({ position = [0, 0, 0] }) {
   </group>
 }
 
-function BlockLimbo ({ position = [0, 0, 0] }) {
+export function BlockLimbo ({ position = [0, 0, 0] }) {
   const obstacle = useRef()
   const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2)
 
@@ -64,7 +65,7 @@ function BlockLimbo ({ position = [0, 0, 0] }) {
   </group>
 }
 
-function BlockAxe ({ position = [0, 0, 0] }) {
+export function BlockAxe ({ position = [0, 0, 0] }) {
   const obstacle = useRef()
   const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2)
 
@@ -86,22 +87,64 @@ function BlockAxe ({ position = [0, 0, 0] }) {
   </group>
 }
 
-function BlockEnd ({ position = [0, 0, 0] }) {
+export function BlockEnd ({ position = [0, 0, 0] }) {
+  const hamburger = useGLTF('./hamburger.glb')
+  hamburger.scene.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true
+    }
+  })
+
   return <group position={ position }>
     {/* Floor */}
     <mesh geometry={ boxGeometry } material={ floor1Material } position-y={ 0 }
       scale={[ 4, 0.2, 4 ]} receiveShadow />
+    <RigidBody type="fixed" colliders="hull" restitution={ 0.2 } friction={ 0 } position={[0, 0.25, 0]}>
+      <primitive object={ hamburger.scene } scale={ 0.2 }/>
+    </RigidBody>
   </group>
 }
 
-export default function Level()
-{
+export function Bounds({ length = 1 }) {
   return <>
-    <BlockStart position={[0, 0, 20]} />
-    <BlockSpinner position={[0, 0, 16]} />
-    <BlockLimbo position={[0, 0, 12]} />
-    <BlockLimbo position={[0, 0, 8]} />
-    <BlockAxe position={[0, 0, 4]} />
-    <BlockEnd position={[0, 0, 0]} />
+    <RigidBody type="fixed" restitution={ 0.2 } friction={ 0 }>
+      {/* Right Wall */}
+      <mesh geometry={ boxGeometry } material={ wallMaterial } position={[ 2.15, 0.75, -(length * 2) + 2 ]}
+        scale={[ 0.3, 1.5, 4 * length ]} receiveShadow castShadow />
+      {/* Left Wall */}
+      <mesh geometry={ boxGeometry } material={ wallMaterial } position={[ -2.15, 0.75, -(length * 2) + 2 ]}
+        scale={[ 0.3, 1.5, 4 * length ]} receiveShadow />
+      {/* Finish Wall */}
+      <mesh geometry={ boxGeometry } material={ wallMaterial } position={[ 0, 0.75, -(length * 4) + 2 ]}
+        scale={[ 4, 1.5, 0.3 ]} receiveShadow castShadow />
+      {/* Floor Collider */}
+      <CuboidCollider
+        args={[ 2, 0.1, 2 * length ]}
+        position={[0, -0.1, -(length * 2) + 2]}
+        restitution={ 0.2 }
+        friction={ 1 }
+      />
+    </RigidBody>
+  </>
+}
+
+export function Level({ count = 5, types = [ BlockSpinner, BlockLimbo, BlockAxe ] }) {
+  const blocks = useMemo(() => {
+    const blocks = []
+
+    for (let i = 0; i < count; i++) {
+      const type = types[Math.floor(Math.random() * types.length)]
+      blocks.push(type)
+    }
+    return blocks
+  }, [ count, types ])
+
+  return <>
+    <BlockStart position={[0, 0, 0]} />
+    {blocks.map((Block, index) => {
+      return <Block key={index} position={[0, 0, -(index + 1) * 4 ]} />
+    })}
+    <BlockEnd position={[0, 0, -(count + 1) * 4]} />
+    <Bounds length={count + 2} />
   </>
 }
